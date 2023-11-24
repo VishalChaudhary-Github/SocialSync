1d7f4a4067b4639a336f22d08bec5ee4512a7670
Vishal Chaudhary <vishalchaudhary5794@gmail.com>
2023-11-25 01:08:27 +0530

    Done

--- Social_Media_Clone/settings.py
+++ Social_Media_Clone/settings.py
@@ -89,7 +89,7 @@ DATABASES = {'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': BASE_DIR / 'db.sqlite3'}
              }
-DATABASES['default'] = dj_database_url.parse(env('INTERNAL'))
+DATABASES['default'] = dj_database_url.parse(env('EXTERNAL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
